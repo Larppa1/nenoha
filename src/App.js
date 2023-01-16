d200ec850943bbf56a641a7693743799ee3ff9ec
larijuva <lari.juva@hoiwa.fi>
2023-01-16 21:32:19 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import Navbar from "./components/Navbar/Navbar"
 import './App.css'
+import { useEffect, useState } from "react"
 
 export default function App() {
+  const [btnColor, setBtnColor] = useState('bg-red-500');
+  const [checked1, setChecked1] = useState(true);
+  const [checked2, setChecked2] = useState(false);
+
   return (
     <div id="App">
       <Navbar />
+      <section className="bannerSection">
+        <img className="banner" src="https://images.hdqwalls.com/wallpapers/abstract-simple-background-4k-lp.jpg" />
+        <h1 className="mainText">Huolelliset oikolukupalvelut niin yksityishenkilöille, kuin myös yrityksille!</h1>
+      </section>
       <main id="Main">
-
+        <h2 className="h2">Palvelut</h2>
+        <section id="Services" className="section">
+          <h3 className="subtitle">Oikolukeminen</h3>
+          <p className="text">Tarjoamme kuluttajille sekä yritysasiakkaille ammattitaitoisia oikolukupalveluita. Oli sitten kyse uutisartikkelista, blogipostauksesta, tai vaikkapa pro gradusta - hoidamme kaikenlaiset oikolukemistyöt huolellisesti. Hinnat määräytyvät sisällön määrän sekä kiireellisyyden mukaan, jolloin sinä päätät, meneekö nopeus vai hinta etusijalle.</p>
+        </section>
+        <h2 className="h2">Ota yhteyttä</h2>
+        <section id="Contact" className="section">
+          <p className="text">Mikäli tarvitsette oikolukemispalvelua, ottakaa meihin yhteyttä alla olevan lomakkeen avulla!</p>
+          <p className="text">Me Nenohalla pyrimme täydelliseen lopputulokseen, mutta aina kaikki eivät mene täysin nappiin. Virheiden ilmetessä voitte käyttää yhteydenottolomaketta myös virheiden ilmoituksiin.</p>
+          <form>
+          <div className="radioSection">
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <span className="label-text">Yhteydenotto</span> 
+                <input id="radio1" type="radio" name="radio-10" className="radio checked:bg-red-500" checked={checked1} onClick={() => {setBtnColor('bg-red-500'); setChecked1(checked1 ? false : true); setChecked2(checked2 ? false : true)}} />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <span className="label-text">Virheilmoitus</span> 
+                <input id="radio2" type="radio" name="radio-10" className="radio checked:bg-blue-500" checked={checked2} onClick={() => {setBtnColor('bg-blue-500'); setChecked2(checked2 ? false : true); setChecked1(checked1 ? false : true)}} />
+              </label>
+            </div>
+          </div>
+          <input type="text" placeholder="Nimi" className="input input-bordered w-full max-w-xs" />
+          <input type="text" placeholder="Sähköposti" className="input input-bordered w-full max-w-xs" />
+          <textarea className="textarea textarea-bordered" placeholder="Viesti"></textarea>
+          <button className={`sendBtn ${btnColor}`}>Lähetä</button>
+        </form>
+        </section>
       </main>
-      <button className="backToTopBtn">Takaisin ylös</button>
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <div>
+          <p className="footerText">2023 - Nenoha / Eetu Ahonen Tmi</p>
+        </div>
+      </footer>
+      <button className="backToTopBtn" onClick={() => {window.scrollTo({top: 0, behavior: 'smooth'})}}>Takaisin ylös</button>
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,15 +3,14 @@ import './Navbar.css'
 export default function Navbar() {
   return (
     <nav id="Navbar">
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 customNav">
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-xl">Nenoha</a>
         </div>
         <div className="flex-4">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Palvelut</a></li>
-            <li><a>Tietoja</a></li>
-            <li><a>Ota yhteyttä</a></li>
+            <li><a onClick={() => {document.getElementById('Services').scrollIntoView({behavior: 'smooth', block: 'end'})}}>Palvelut</a></li>
+            <li><a onClick={() => {document.getElementById('Contact').scrollIntoView({behavior: 'smooth', block: 'end'})}}>Ota yhteyttä</a></li>
           </ul>
         </div>
       </div>
